e0f7a09b4122b39fac3ac0f40fed9c1103dffe70
Jake Weinstein <xboxlover360@gmail.com>
2015-07-17 02:42:48 +0000

    MusicFX: enable by default
    * It sounds muy bueno.

--- src/com/android/musicfx/ControlPanelEffect.java
+++ src/com/android/musicfx/ControlPanelEffect.java
@@ -110,7 +110,7 @@ public class ControlPanelEffect {
             HASHMAP_INITIAL_CAPACITY, HASHMAP_LOAD_FACTOR, HASHMAP_CONCURRENCY_LEVEL);
 
     // Defaults
-    final static boolean GLOBAL_ENABLED_DEFAULT = false;
+    final static boolean GLOBAL_ENABLED_DEFAULT = true;
     private final static boolean VIRTUALIZER_ENABLED_DEFAULT = true;
     private final static int VIRTUALIZER_STRENGTH_DEFAULT = 1000;
     private final static boolean BASS_BOOST_ENABLED_DEFAULT = true;
